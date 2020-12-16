143f864a37fa49be1df9fa9dfa17b941fdf2b458
CHEN Dongmei <DCHEN@lcog.org>
2020-12-15 20:34:16 -0800

    completed VMT_Pop plots

--- PopulationData/VMT_Pop.r
+++ PopulationData/VMT_Pop.r
@@ -4,5 +4,77 @@
 # On December 15, 2020
 
 # load libraries
-library(readxl)
+library(xlsx)
+
+vmt_path <- "T:/Data/VMT/VMT_State_and_County"
+pop_path <- "T:/Tableau/tableauPop/Datasources"
+
+############################### Read VMT data ###########################
+# open the table in Excel to check the reading range
+vmt_state <- read.xlsx(file = file.path(vmt_path, "VMT_State.xls"),
+                       sheetName = "VMT Web Publication", colIndex = 1:8,
+                       startRow = 5, endRow = 24, header = FALSE)
+head(vmt_state)
+# rearrange the columns
+for(i in 1:(dim(vmt_state)[2]/2)){
+  if(i==1){
+    cols <- vmt_state[,i:(2*i)]
+    colnames(cols) <- c("Year", "VMT")
+  }else{
+    new_cols <- vmt_state[,((2*i)-1):(2*i)]
+    colnames(new_cols) <- c("Year", "VMT")
+    cols <- rbind(cols, new_cols)
+  }
+}
+vmt_state_re <- cols
+colnames(vmt_state_re)[2] <- "State_VMT"
+vmt_state <- vmt_state_re[44:72,]
+
+vmt_county <- read.xlsx(file = file.path(vmt_path, "VMT_County.xls"),
+                       sheetName = "VMT by County", colIndex = 1:30,
+                       startRow = 5, endRow = 41, header = FALSE)
+vmt_county <- vmt_county[vmt_county$X1 %in% c("COUNTY", "LANE"),]
+vmt_county <- data.frame(Year = as.numeric(vmt_county[1, 2:30]),
+                         VMT = as.numeric(vmt_county[2, 2:30]))
+
+vmt_county <- vmt_county[order(vmt_county$Year),]
+colnames(vmt_county)[2] <- "County_VMT"
+
+vmt <- cbind(vmt_state, vmt_county)[,-3]
+vmt <- as.data.frame(apply(vmt, 2, as.numeric))
+
+############################### Read Pop data ###########################
+hist_pop <- read.xlsx(file = file.path(pop_path, "HistoricalPopulation.xlsx"),
+                       sheetName = "PopulationRaw", colIndex = 1:7)
+vars <- c("Year", "Pop", "GrowthRate")
+colnames(hist_pop)[which(colnames(hist_pop) %in% 
+                           c("YEAR", "POPULATION", "X5.YEAR.AVG..ANNUAL.GROWTH.RATE"))] <- vars
+state_pop <- subset(hist_pop, as.numeric(Year) >= 1991 & GEOGRAPHY == "Oregon")[,vars]
+county_pop <- subset(hist_pop, as.numeric(Year) >= 1991 & GEOGRAPHY == "Lane County (Total)")[,vars]
+colnames(state_pop)[2:3] <- paste0("State_", colnames(state_pop)[2:3])
+colnames(county_pop)[2:3] <- paste0("County_", colnames(county_pop)[2:3])
+
+pop <- cbind(state_pop, county_pop[,2:3])
+
+############################### Export the data ###########################
+
+data <- cbind(vmt, pop[,-1])
+geos <- c("State", "County")
+vars <- c("PerCap", "AnnGR")
+types <- c("VMT", "Pop")
+
+for(geo in geos){
+  for(var in vars){
+    if(var == "PerCap"){
+      data[,paste0(geo, "_", var)] <- data[,paste0(geo, "_VMT")]/data[,paste0(geo, "_Pop")]
+    }else{
+      for(type in types){
+        x <- data[,paste0(geo, "_", type)]
+        n <- length(x)
+        data[,paste0(geo, "_", type, "_", var)] <- c(NA, x[2:n]/x[1:(n-1)]-1)
+      }
+    }
+  }
+}
+write.csv(data, file.path(pop_path, "VMT_Pop.csv"), row.names = FALSE)
 
