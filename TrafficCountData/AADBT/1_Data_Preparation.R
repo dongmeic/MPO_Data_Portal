d7920fd2ea25fd957a5b020b3126a3045dd88d7a
CHEN Dongmei <DCHEN@lcog.org>
2022-10-05 23:23:02 -0700

    redo noaa download

--- TrafficCountData/AADBT/1_Data_Preparation.R
+++ TrafficCountData/AADBT/1_Data_Preparation.R
@@ -6,6 +6,8 @@ library(rjson)
 library(rnoaa)
 library(lubridate)
 library(stringr)
+library(geosphere)
+library(dplyr)
 
 # functions
 # weight by distance
@@ -23,6 +25,12 @@ idw <- function(lon, lat, lonv, latv, varv){
 # summary for weights
 sumidw <- function(distv){
   sum(sapply(distv, function(x) 1/x))
+}
+
+# distance
+distsq <- function(lon1, lon2, lat1, lat2){
+  #(lon1-lon2)^2+(lat1-lat2)^2
+  (distm(c(lon1, lat1), c(lon2, lat2), fun = distVincentyEllipsoid))^2
 }
 
 options(warn = -1)
@@ -47,10 +55,6 @@ aggdata <- aggregate(x=list(DailyCounts = data$Hourly_Count),
                              Location = data$Location), 
                      FUN=sum, na.rm=TRUE)
 
-no_days <- aggregate(x=list(ObsDays = data$Date), 
-                     by=list(Location = data$Location), 
-                     FUN=function(x) length(unique(x)))
-
 datedata <- unique(data[,c("Date", "Month", "MonthDesc", "Season", "Weekday", 
                            "IsHoliday", "UoInSession", "IsSpecialEvent")])
 aggdata <- merge(aggdata, datedata, by="Date")
@@ -73,7 +77,7 @@ loc_data <- data.frame()
 
 locations <- unique(aggdata$Location)
 # need to manually fix error - Error: Bad Gateway (HTTP 502)
-loc <- "AlderSouthFranklin"
+loc <- "RosaParksPathSouthQ"
 locations <- locations[which(locations==loc):length(locations)]
 for(location in locations){
   ptm <- proc.time()
@@ -102,7 +106,7 @@ for(location in locations){
 
   print(paste("Check climate data for", location, "from", Start_Date, 
               "to", End_Date, paste0("(", n, " years)")))
-  clim_loc <- data.frame()
+
   for(year in years){
     if(year == years[1]){
       StartDate <- paste0(year, "-",str_pad(month(Start_Date), 2, pad="0"),"-01")
@@ -116,7 +120,7 @@ for(location in locations){
     }
     
     for(dt in c("PRCP","TMAX","SNOW")){
-      station_ids <- paste0('GHCND:', locdata$id)
+      station_ids <- paste0('GHCND:', locdata$id)y
       # in case missing data
       station_ids <- c(station_ids, "GHCND:USW00024221")
       for(station_id in station_ids){
@@ -127,15 +131,50 @@ for(location in locations){
                      enddate = EndDate,
                      add_units = T)$data
         clim$location <- location
-        noaa_data<- rbind(noaa_data,clim)
-        clim_loc <- rbind(clim_loc, clim)
+        test <- rbind(test, clim)
+        #noaa_data <- rbind(noaa_data,clim) # the loop is too long to run
         print(paste(dt, station_id, "from", StartDate, "to", EndDate, "for", location))
       }
     }
   }
   print(paste("Got climate data for", location))
-  write.csv(locdata, paste0(outpath, "noaa_stations_", location, ".csv"), row.names = FALSE)
-  write.csv(clim_loc, paste0(outpath, "clim_data_", location, ".csv"), row.names = FALSE)
   print(proc.time() - ptm)
+}
+
+# remove the duplicated due to the 502 Bad Gateway Error
+noaa_data <- unique(noaa_data)
+loc_data <- unique(loc_data)
+write.csv(loc_data, paste0(outpath, "noaa_stations.csv"), row.names = FALSE)
+write.csv(noaa_data, paste0(outpath, "noaa_data.csv"), row.names = FALSE)
+
+noaa_data$date <- unlist(lapply(noaa_data$date, function(x) strsplit(x, split = "T")[[1]][1]))
+noaa_data$station <- unlist(lapply(noaa_data$station, function(x) strsplit(x, split = ":")[[1]][2]))
+
+names(loc_data)[1] <- "station"
+
+for(location in locations){
+  dates <- as.character(sort(as.Date(aggdata[aggdata$Location == location, 'Date'], "%Y-%m-%d")))
+  for(date in dates){
+    for(dtype in c("PRCP","TMAX","SNOW")){
+      clim <- noaa_data[noaa_data$location == location & noaa_data$date == date & noaa_data$datatype == dtype,]
+      if(dim(clim)[1] > 1){
+        locdt <- loc_data[loc_data$location == location & loc_data$station %in% clim$station,]
+        if(dim(locdt)[1] == 1){
+          # choose the data from the close station
+          aggdata[aggdata$Location == location & aggdata$Date == date, dtype] <- clim[clim$station == locdt$station,]$value
+        }else{
+          
+        }
+          
+      }else if(dim(clim)[1] == 1){
+        # when there is only one value
+        aggdata[aggdata$Location == location & aggdata$Date == date, dtype] <- clim$value
+      }else{
+        # look for data from other stations
+      }
+    }
+      
+      
+  }
 }
 
