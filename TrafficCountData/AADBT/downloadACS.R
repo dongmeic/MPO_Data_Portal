83accde1502cbd9ef20eeb4f2f6e5b1758797584
dongmeic <dongmeic@uoregon.edu>
2022-11-09 16:45:37 -0800

    download multi-year ACS

---
+++ TrafficCountData/AADBT/downloadACS.R
@@ -0,0 +1,42 @@
+library(rjson)
+library(tidycensus)
+
+
+keypath <- "T:/DCProjects/GitHub/MPO_Data_Portal/TrafficCountData/AADBT/"
+census_api_key(rjson::fromJSON(file=paste0(keypath, "config/keys.json"))$acs$key, install = TRUE)
+
+# place to store results and combine them
+# Block groups are not currently available for the 2008-2012 ACS and earlier.
+years <- 2013:2020
+res <- vector("list",length(years))
+names(res) <- years
+
+# variables that you want
+#        Tot Pop     White non-Hisp  FemHeadHouse  FamPoverty
+vars <- c('B01001_001','B03002_003','B11003_016','B17010_002')
+
+# loop over years, save data
+# could also apply county filter, see help(get_acs)
+for (y in years){
+  # download data
+  ld <- as.data.frame(get_acs(year = y,
+                              geography='cbg',
+                              survey='acs5',
+                              variables = vars,
+                              state="OR",
+                              county = "Lane"))
+  # reshape long to wide
+  ld2 <- reshape(ld,
+                 idvar="GEOID",
+                 timevar="variable",
+                 direction="wide",
+                 drop=c("NAME","moe"))
+  # insert into list and add in year
+  res[[y]] <- ld2
+  res[[y]]$year <- y
+}
+
+# Combining the data frames together for final analysis
+combo <- do.call("rbind",res)
+head(combo) 
+summary(combo)
